de1e573709899d855995dfa037dd5090ca9262fb
Jayne Hsu <jaynehsu@Jaynes-MBP.hsd1.ca.comcast.net>
2018-08-31 09:43:07 -0700

    wip

--- cb.5.js
+++ cb.5.js
@@ -1,9 +1,16 @@
   function returnAValue (callback) {
+    console.log('entering');
+    setTimeout(() => {
         if (Math.random() < 0.10) {
+          console.log('111');
           callback(new Error('ops'));
+          console.log('222');
         } else {
+          console.log('333');
           return callback(null, Math.floor(Math.random() * 10));
+          console.log('444');
         }
+      }, 1000);
   }
 
   function handleValueResult(err, result){
@@ -17,7 +24,9 @@
   function addNRandomNumbers(n, callback){
     let total = 0;
     while(n>0){
+      console.log('aaa');
       let num = returnAValue(callback);
+      console.log('bbb');
       if(num){
         total = total + num;
         n--;
@@ -34,6 +43,9 @@
    
 //   // Build function AddNRandomNumbers(n, callback) that return the result of 
 //  the addition of n numbers (you get them with returnAValue)
+
+// http://callbackhell.com/
+// https://stackoverflow.com/questions/10312963/difference-between-settimeout-with-and-without-quotes-and-parentheses
   
   
 
